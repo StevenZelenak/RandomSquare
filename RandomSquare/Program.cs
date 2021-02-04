45865d3ff5f9e84539bcdd16784f108328c02591
Steven Zelenak <stevenmzelenak@gmail.com>
2021-02-04 17:40:34 -0600

    Adding RandomSquare

---
+++ RandomSquare/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RandomSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create a loop that adds random numbers between 1-50
+            Random rnd = new Random();
+            var randomNumbers = new List<int>();
+            var numbersSquared = new List<int>();
+            var numbersSquaredEven = new List<int>();
+
+            //create list of random Numbers
+            for (int i = 0; i < 20; i++)
+            {
+                randomNumbers.Add(rnd.Next(1,50));
+                
+            }
+
+            //square the numbers and add them to a list
+            foreach (var number in randomNumbers)
+            {
+                numbersSquared.Add(number * number);
+            }
+
+            //Take the new squared list and remove all the odd numbers
+            foreach (var numbersquared in numbersSquared)
+            {
+                if ( numbersquared % 2 == 0)
+                {
+                    numbersSquaredEven.Add(numbersquared);
+                }
+                
+            }
+            
+            //Output the squared even numbers
+            foreach (var numbersquaredeven in numbersSquaredEven)
+            {
+                Console.WriteLine(numbersquaredeven.ToString());
+            } 
+        }
+    }
+}
